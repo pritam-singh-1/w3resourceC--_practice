5835d18963f65eda787ad5fa9fbc042b51b429b2
Pritam Singh <pritamsingh2511p@gmail.com>
2026-01-30 08:20:01 +0530

    Either the number 30 or sum of them is 30

---
+++ 3_Sum30.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    int c=a+b;
+    cout<<"Enter first number: "<<endl;
+    cin>>a;
+    cout<<"Enter second number "<<endl;
+    cin>>b;
+    // cout<<"Enter third number: "<<endl;
+    // cin>>c;
+    if(a==30 || b==30 || c==30){
+        // cout<<"1"<<endl;
+        return 1;
+
+    }
+    // else if(c==30){
+    //     cout<<"1"<<endl;
+    // }
+    else{
+        // cout<<"0"<<endl;
+        return 0;
+    }
+    // return 0;
+
+}
