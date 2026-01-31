7988dde23a26dc3b48e3c3012309c73e2d5dc84a
Pritam Singh <pritamsingh2511p@gmail.com>
2026-01-31 10:43:16 +0530

    Erasing element of at position from a string

---
+++ 6_RemoveOnPos.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter a string: "<<endl;
+    getline(cin,s);
+    cout<<"Enter the position: "<<endl;
+    int x;
+    cin>>x;
+    cout<<s.erase(x-1,1)<<endl;
+    return 0;
+
+}
