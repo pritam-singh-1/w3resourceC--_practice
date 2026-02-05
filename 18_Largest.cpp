cf2e54afa3d96478f8b935df8e02230899568766
Pritam Singh <pritamsingh2511p@gmail.com>
2026-02-05 23:50:55 +0530

    Largest number out of three

---
+++ 18_Largest.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int largest(int a, int b,int c)
+{
+    if(a>b && a>c)
+        return a;
+    else if(b>a && b>c)
+        return b;
+    else
+        return c;
+}
+int main(){
+    int a,b,c;
+    cout<<"Enter three numbers: "<<endl;
+    cin>>a>>b>>c;
+    cout<<"The largest number is: "<<largest(a,b,c)<<endl;
+    return 0;
+}
